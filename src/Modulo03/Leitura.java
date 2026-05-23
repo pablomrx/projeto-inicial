4e77fb5bc865ead86db0d9b64559212619846514
Pablo Marques <pablorunescape09@gmail.com>
2026-05-23 17:54:09 -0300

    complete third java module

---
+++ src/Modulo03/Leitura.java
@@ -0,0 +1,22 @@
+package Modulo03;
+
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Diga sua avaliação para o filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println("Filme: " + filme);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Avaliação do filme: " + avaliacao);
+    }
+}
---
+++ src/Modulo03/Loop.java
@@ -0,0 +1,19 @@
+package Modulo03;
+
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga sua avaliação para o filme: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.printf("Média de avaliações: %.2f%n", mediaAvaliacao / 3);
+    }
+}
