7bc9599bb8f6e026b263aa562fa3f7a37ea646be
Pablo Marques <pablorunescape09@gmail.com>
2026-05-23 12:04:56 -0300

    complete first java module

---
+++ src/Modulo01/Perfil.java
@@ -0,0 +1,10 @@
+package Modulo01;
+
+public class Perfil {
+    public static void main(String[] args) {
+        String nome = "Pablo";
+        String mensagem = "Tudo bem?";
+        System.out.println("Olá, " + nome);
+        System.out.println(mensagem);
+    }
+}
---
+++ src/Modulo01/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package Modulo01;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
